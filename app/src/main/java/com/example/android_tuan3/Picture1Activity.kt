0aae56a70c4871a236d4fe8f7e0f576c27c1bf1b
Phat999 <64886764+Phat999@users.noreply.github.com>
2021-04-14 17:40:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android_tuan3/Picture1Activity.kt
@@ -0,0 +1,11 @@
+package com.example.android_tuan3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Picture1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_picture_1)
+    }
+}
